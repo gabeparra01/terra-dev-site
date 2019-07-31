7808f1f479f283c39588bfdf87d730fa82008376
Derek Yu <yuderekyu@gmail.com>
2019-07-31 09:05:47 -0500

    incorporate scoped theme updates

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,9 +1,10 @@
 const merge = require('webpack-merge');
 const defaultWebpackConfig = require('terra-toolkit/config/webpack/webpack.config');
 const terraDevSiteConfig = require('./terra-dev-site.webpack.config');
-
+const themeConfig = require('../theme/theme-config');
 
 const mergedConfig = (env, argv) => (
+  Object.assign(env, themeConfig);
   merge.strategy({
     'resolve.modules': 'prepend',
   })(defaultWebpackConfig(env, argv), terraDevSiteConfig(env, argv))
--- terra-theme.config.js
+++ terra-theme.config.js
@@ -1,7 +1,8 @@
 const themeConfig = {
   exclude: [], // Files to exclude. Accepts glob patterns.
   theme: '', // The default theme.
-  scoped: ['terra-dev-site-test-theme'], // An array of scoped themes.
+  generateScopedThemes: true,
+  scoped: [{ name: 'terra-dev-site-test-theme' }] // An array of scoped themes.
 };
 
 module.exports = themeConfig;
